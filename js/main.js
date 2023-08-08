5872a9f01f8b18252c2d0f4b368376c0e7901191
Michele Ascoli <micheleascoli17@gmail.com>
2023-08-08 16:40:06 -0500

    montando uma peça

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 
-const robotron =  document.querySelector("#robotron")
+const robotron =  document.querySelector("#robotron");
 
+
+const subtrair =  document.querySelector("#subtrair");
+const somar =  document.querySelector("#somar");
+const braco =  document.querySelector("#braco");
+
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach( (elemento) =>{
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent);
+        console.log(evento);
+    })
+})
+
+
+function manipulaDados(operacao, parteCorpo) {
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value)-1;
+    } else {
+        braco.value = parseInt(braco.value)+1;
+    }
+}
+
+
+/*************************** */
 robotron.addEventListener("click", (evento) => {
     console.log(evento);
 })
