c61ef2a17806c0617bd88692362bca9d49f5a1d2
Michele Ascoli <micheleascoli17@gmail.com>
2023-08-09 12:53:12 -0500

    montando o robo

--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
-
-const robotron =  document.querySelector("#robotron");
-
-
-const subtrair =  document.querySelector("#subtrair");
-const somar =  document.querySelector("#somar");
-const braco =  document.querySelector("#braco");
-
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach( (elemento) =>{
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent);
-        console.log(evento);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     })
 })
 
-
-function manipulaDados(operacao, parteCorpo) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
     if(operacao === "-"){
-        braco.value = parseInt(braco.value)-1;
+        peca.value = parseInt(peca.value)-1;
     } else {
-        braco.value = parseInt(braco.value)+1;
+        peca.value = parseInt(peca.value)+1;
     }
 }
-
-
-/*************************** */
-robotron.addEventListener("click", (evento) => {
-    console.log(evento);
-})
-
-function dizOi(nome) {
-    console.log("oi");
-    console.log(nome);
-    console.log("Bem-vindo ao Robotron 2000");
-}
-
-dizOi();
